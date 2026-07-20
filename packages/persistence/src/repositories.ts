a822b8929b1512cc5eec226b3eb28336e0e0afd4
Sentionce <crazytaxzi@gmail.com>
2026-07-19 21:23:13 -0700

    Export Stage 04 HOS persistence

--- packages/persistence/src/repositories.ts
+++ packages/persistence/src/repositories.ts
@@ -1,6 +1,7 @@
 import type { JsonObject } from './json.js';
 import { toJsonObject } from './json.js';
 
+export * from './driver-hos-repository.js';
 export * from './export-history-repository.js';
 export * from './regulatory-rule-repository.js';
 export * from './route-provider-response-repository.js';
