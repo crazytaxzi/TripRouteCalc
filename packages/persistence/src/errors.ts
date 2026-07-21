f0290ddb3113d0b3b4e9dd419c5aee3ae462c09b
Sentionce <crazytaxzi@gmail.com>
2026-07-21 10:02:03 -0700

    Add Stage 17 persistence conflict errors

--- packages/persistence/src/errors.ts
+++ packages/persistence/src/errors.ts
@@ -17,3 +17,28 @@ export class ImmutableSnapshotError extends Error {
 export class ProviderEvidenceError extends Error {
   public override readonly name = 'ProviderEvidenceError';
 }
+
+export class TripRevisionConflictError extends Error {
+  public override readonly name = 'TripRevisionConflictError';
+
+  public constructor(
+    message: string,
+    public readonly expectedRevisionNumber?: number,
+    public readonly actualRevisionNumber?: number,
+    options?: ErrorOptions,
+  ) {
+    super(message, options);
+  }
+}
+
+export class IdempotencyConflictError extends Error {
+  public override readonly name = 'IdempotencyConflictError';
+
+  public constructor(
+    message: string,
+    public readonly reason: 'REQUEST_MISMATCH' | 'IN_PROGRESS',
+    options?: ErrorOptions,
+  ) {
+    super(message, options);
+  }
+}
